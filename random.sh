ac85c446d2552c9e03d5b9bd22ba64e7d2e442c4
Valto Kristian S <kbvalto@cubbli18-gold.local>
2019-11-12 17:57:20 +0200

    scripts2

--- random.sh
+++ random.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-LENGTH=$(shuf -i 1-100000 -n 1)
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "$LENGTH" | head -n 1)
-echo sdfsfd
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "$2" | head -n 1)
 RES=$(curl -d '{"type":"'"$1"'", "msg":"'"$NEW_UUID"'"}' -H "Content-Type: application/json" -X POST http://localhost:8080)
 
 echo $RES
